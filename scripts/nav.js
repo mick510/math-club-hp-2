8aac55b15da73527221ece8bacf6ea5e9fabe97f
M's <msarchives314@gmail.com>
2026-09-18 21:47:41 -0600

    template-v7

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,25 +2,46 @@
 const openSideNav=document.getElementsByClassName('open-side-nav')[0];
 const smallNav=document.getElementsByClassName('small-nav')[0];
 
-openSideNav.addEventListener('click', function(event) {
-    event.stopPropagation();
-    smallNav.style.display = 'flex';
+let checkOpenSideNav;
+
+openSideNav.addEventListener('click', function() {
+    checkOpenSideNav=smallNav.classList.toggle('open');
 });
 
 document.addEventListener('click', function(event) {
-    if (!smallNav.contains(event.target)) {
-        smallNav.style.display = 'none';
+    if (
+        checkOpenSideNav && 
+        !smallNav.contains(event.target) &&
+        !openSideNav.contains(event.target)
+    ) {
+        checkOpenSideNav=smallNav.classList.toggle('open');
     }
 });
 
 //toggle display of menu options under resources
 const menuOptionsToggle=document.getElementsByClassName('button-menu-options')[0];
 const menuOptions=document.getElementsByClassName('side-nav-menu-options')[0];
+const triangle=document.getElementsByClassName('triangle')[0];
+
+let checkOpenMenuOptions;
 
 menuOptionsToggle.addEventListener('click', function() {
-    if (menuOptions.style.display=='none') {
-        menuOptions.style.display='flex';
+    checkOpenMenuOptions=menuOptions.classList.toggle('open');
+    if (checkOpenMenuOptions) {
+        triangle.style.transform = 'rotate(90deg)';
     } else {
-        menuOptions.style.display='none'
+        triangle.style.transform = 'rotate(0deg)';
+    }
+});
+
+//toggle everything off when we get big header due to window size
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 650&&checkOpenSideNav) {
+        checkOpenSideNav=smallNav.classList.toggle('open');
+    }
+
+    if (checkOpenMenuOptions) {
+        checkOpenMenuOptions=menuOptions.classList.toggle('open');
+        triangle.style.transform = 'rotate(0deg)';
     }
 });
