e6b76d8316b0e94c78821df7107ab14611d2a90f
M's <msarchives314@gmail.com>
2026-09-18 20:26:26 -0600

    template-v6

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,26 @@
-const button=document.getElementsByClassName("open-side-nav")[0];
-const content=document.getElementsByClassName("small-nav")[0];
+//toggle display of side-nav between none and flex
+const openSideNav=document.getElementsByClassName('open-side-nav')[0];
+const smallNav=document.getElementsByClassName('small-nav')[0];
 
-button.addEventListener('click', function() {
-    content.style.display='flex';
+openSideNav.addEventListener('click', function(event) {
+    event.stopPropagation();
+    smallNav.style.display = 'flex';
+});
+
+document.addEventListener('click', function(event) {
+    if (!smallNav.contains(event.target)) {
+        smallNav.style.display = 'none';
+    }
+});
+
+//toggle display of menu options under resources
+const menuOptionsToggle=document.getElementsByClassName('button-menu-options')[0];
+const menuOptions=document.getElementsByClassName('side-nav-menu-options')[0];
+
+menuOptionsToggle.addEventListener('click', function() {
+    if (menuOptions.style.display=='none') {
+        menuOptions.style.display='flex';
+    } else {
+        menuOptions.style.display='none'
+    }
 });
